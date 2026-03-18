0dde5dc6d73508239d45c7380885a85f81041bf7
ritikkumar27 <rkv2771@gmail.com>
2026-03-18 14:07:49 +0530

    db: schema done

---
+++ drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'drizzle-kit';
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: 'postgres://blog_user:blog_password@localhost:5432/blog_db',
+  },
+});
