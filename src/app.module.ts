899eff1c853a5406cf576436d4aaded7fbd711dd
ritikkumar27 <rkv2771@gmail.com>
2026-03-18 14:51:36 +0530

    code: comment module initiated

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostsModule } from './modules/posts/posts.module';
+import { CommentsModule } from './modules/comments/comments.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, CommentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
