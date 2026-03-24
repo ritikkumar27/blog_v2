d81047542c043adda4cd486f86142f22271f95f1
ritikkumar27 <rkv2771@gmail.com>
2026-03-24 23:22:59 +0530

    code: auth service and auth module

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  
+  private readonly ADMIN_USERNAME = 'ritik';
+  private readonly ADMIN_PASSWORD = 'changethislater123';
+
+  constructor(private jwtService: JwtService) {}
+
+  async login(username: string, password: string) {
+    if (username !== this.ADMIN_USERNAME || password !== this.ADMIN_PASSWORD) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    
+    const token = await this.jwtService.signAsync({ username, role: 'admin' });
+    return token;
+  }
+}
