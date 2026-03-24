bb3cbff171aa4a685d47a7af1692d31548170a9e
ritikkumar27 <rkv2771@gmail.com>
2026-03-24 23:19:12 +0530

    code: files for auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './modules/posts/posts.module';
 import { CommentsModule } from './modules/comments/comments.module';
 import { WebModule } from './modules/web/web.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 @Module({
-  imports: [PostsModule, CommentsModule, WebModule],
+  imports: [PostsModule, CommentsModule, WebModule, AuthModule, AdminModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
